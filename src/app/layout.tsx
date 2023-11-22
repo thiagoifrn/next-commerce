2e0e91deb3c9e537f6abe3f43fc8a79d759362f2
thiagoifrn <thiagoifrn@gmail.com>
2023-11-22 00:06:40 -0300

    chore: styling product component with next image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,9 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={clsx(inter.className, "bg-slate-700")}>
         <Navbar />
-        <main className="bg-slate-700 h-screen p-16">{children}</main>
+        <main className=" h-screen p-16">{children}</main>
       </body>
     </html>
   );
