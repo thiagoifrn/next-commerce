29dfba02aebaabe5c5208eaef816487714bf4432
thiagoifrn <thiagoifrn@gmail.com>
2023-11-21 21:55:48 -0300

    list products part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Next E-commerce 13</h1>
-    </main>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:p-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 ">
+        <div className="bg-white rounded-lg overflow-hidden">Prod 1</div>
+        <div className="bg-white rounded-lg overflow-hidden">Prod 2</div>
+        <div className="bg-white rounded-lg overflow-hidden">Prod 3</div>
+        <div className="bg-white rounded-lg overflow-hidden">Prod 4</div>
+      </div>
+    </div>
   );
 }
