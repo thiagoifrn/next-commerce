08804901ab716da7c2bb376eb941f20af8df9f88
thiagoifrn <thiagoifrn@gmail.com>
2023-11-21 22:39:14 -0300

    feat: define type for products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-export default function Home() {
+import { ProductType } from "./types/ProductType";
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:p-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 ">
-        <div className="bg-white rounded-lg overflow-hidden">Prod 1</div>
-        <div className="bg-white rounded-lg overflow-hidden">Prod 2</div>
-        <div className="bg-white rounded-lg overflow-hidden">Prod 3</div>
-        <div className="bg-white rounded-lg overflow-hidden">Prod 4</div>
+        {products.map((product: ProductType) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
       </div>
     </div>
   );
