57adfe15e00258ee71ff18063b149cc79bcf9827
thiagoifrn <thiagoifrn@gmail.com>
2023-11-21 23:29:01 -0300

    add product component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Product from "./components/Products";
 import { ProductType } from "./types/ProductType";
 async function getProducts() {
   const res = await fetch("https://fakestoreapi.com/products");
@@ -12,12 +13,11 @@ async function getProducts() {
 
 export default async function Home() {
   const products = await getProducts();
-  console.log(products);
   return (
-    <div className="max-w-7xl mx-auto pt-8 px-8 xl:p-0">
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 ">
         {products.map((product: ProductType) => (
-          <div key={product.id}>{product.title}</div>
+          <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
