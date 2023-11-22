22f9fb9d2a56ea572ece1177afaddb1014635c74
thiagoifrn <thiagoifrn@gmail.com>
2023-11-22 01:15:04 -0300

    add clerk auth

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 function Navbar() {
@@ -9,6 +10,18 @@ function Navbar() {
       >
         Next Store
       </Link>
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="border rounded-md border-gray-400 px-3 py-2">
+              Fazer Login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </nav>
   );
 }
